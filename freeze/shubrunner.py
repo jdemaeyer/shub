f0b493a400fa881313a2b764e57fabf8feaf473a
Jakob de Maeyer <jakobdm1@gmail.com>
2016-01-20 20:28:47 +0000

    Suppress warnings in binary versions

--- freeze/shubrunner.py
+++ freeze/shubrunner.py
@@ -1,4 +1,11 @@
+import warnings
+
 import shub.tool
+
+
+# Suppress all warnings for the time being (avoid PEP425 RuntimeWarnings)
+# TODO: Investigate why they occur in the first place
+warnings.simplefilter('ignore')
 
 # Run cli
 shub.tool.cli()
