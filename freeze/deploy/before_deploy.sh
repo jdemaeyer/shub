6da4ff0720157b689db32bcd2a7f05154cfaf6ad
Jakob de Maeyer <jakobdm1@gmail.com>
2016-01-20 13:16:31 +0000

    Move shub binary tarring to travis.yml

--- freeze/deploy/before_deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Create archive dist/shub-linux.tar.gz (or dist/shub-osx.tar.gz) containing
-# only the shub binary (and no directory structure)
-tar -czf dist/shub-${TRAVIS_TAG}-${TRAVIS_OS_NAME}.tar.gz -C dist shub
