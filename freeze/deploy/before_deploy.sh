f4a63c8950fdcf809b10781c593381b3e16e8d3b
Jakob de Maeyer <jakobdm1@gmail.com>
2016-01-20 12:52:37 +0000

    Fix being an idiot

--- freeze/deploy/before_deploy.sh
+++ freeze/deploy/before_deploy.sh
@@ -2,4 +2,4 @@
 
 # Create archive dist/shub-linux.tar.gz (or dist/shub-osx.tar.gz) containing
 # only the shub binary (and no directory structure)
-tar -czf dist/shub-${TRAVIS_OS_NAME}.tar.gz -C dist shub
+tar -czf dist/shub-${TRAVIS_TAG}-${TRAVIS_OS_NAME}.tar.gz -C dist shub
