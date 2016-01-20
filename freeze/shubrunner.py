596a6e91a1d9198147f19ed46fbb1bffac5f278e
Jakob de Maeyer <jakobdm1@gmail.com>
2016-01-20 20:54:29 +0000

    Suppress pip RuntimeWarnings

--- freeze/shubrunner.py
+++ freeze/shubrunner.py
@@ -3,9 +3,10 @@ import warnings
 import shub.tool
 
 
-# Suppress all warnings for the time being (avoid PEP425 RuntimeWarnings)
-# TODO: Investigate why they occur in the first place
-warnings.simplefilter('ignore')
+# Suppress RuntimeWarnings caused by pip 8.0.0 on Windows
+# https://github.com/pypa/pip/issues/3383
+# XXX: We should be able to remove this once pip 8.0.1 is published
+warnings.simplefilter('ignore', RuntimeWarning)
 
 # Run cli
 shub.tool.cli()
