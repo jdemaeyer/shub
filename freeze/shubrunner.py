20d468c4bec376b0df6739c1c545d908c1cdd049
Jakob de Maeyer <jakobdm1@gmail.com>
2016-01-20 21:09:23 +0000

    Revert shubrunner

--- freeze/shubrunner.py
+++ freeze/shubrunner.py
@@ -1,12 +1,4 @@
-import warnings
-
 import shub.tool
-
-
-# Suppress RuntimeWarnings caused by pip 8.0.0 on Windows
-# https://github.com/pypa/pip/issues/3383
-# XXX: We should be able to remove this once pip 8.0.1 is published
-warnings.simplefilter('ignore', RuntimeWarning)
 
 # Run cli
 shub.tool.cli()
